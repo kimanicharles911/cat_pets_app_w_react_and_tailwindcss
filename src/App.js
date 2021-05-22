61632b211754886ec6b3c8475b57d8b67f3019bf
kimanicharles911 <kimanicharles911@gmail.com>
2021-05-22 12:22:32 +0300

    fetching without authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const fetchFunc = (breedInput) => {
+    fetch(`https://api.thecatapi.com/v1/images/search?breed_id=${breedInput}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  }
+  fetchFunc("beng")
+
   return (
     <div className="App">
-      
+      <p>Hello world</p>
     </div>
   );
 }
