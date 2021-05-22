fc9bea22aea39fce9c820725c7baa1df96eb06d8
kimanicharles911 <kimanicharles911@gmail.com>
2021-05-22 14:46:22 +0300

    fetching with authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import "./App.css";
 
-function App() {
-  const fetchFunc = (breedInput) => {
-    fetch(`https://api.thecatapi.com/v1/images/search?breed_id=${breedInput}`)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-  }
-  fetchFunc("beng")
+const App = () => {
+  const token = process.env.REACT_APP_MY_TOKEN;
+
+  let req = new Request(`https://api.thecatapi.com/v1/images/search?breed_id=beng`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": token,
+    },
+    mode: "cors",
+  });
+
+  const fetchFunc = (req) => {
+    fetch(req)
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  };
+  fetchFunc(req);
 
   return (
     <div className="App">
       <p>Hello world</p>
     </div>
   );
-}
+};
 
 export default App;
