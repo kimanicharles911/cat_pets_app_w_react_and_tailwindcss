827731870c535f159de7f5601b52dd6e8335ba7f
kimanicharles911 <kimanicharles911@gmail.com>
2021-05-23 16:02:33 +0300

    test TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import "./App.css";
 import { useState, useEffect } from "react";
 
 const App = () => {
   const token = process.env.REACT_APP_MY_TOKEN;
 
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState(`https://cdn2.thecatapi.com/images/MTc5OTc4Ng.jpg`);
 
-  useEffect(() => {
+  /* useEffect(() => {
     handleFetch();
-  }, []);
+  }, []); */
 
   const handleFetch = () => {
     fetch(`https://api.thecatapi.com/v1/images/search`, {
@@ -30,10 +29,33 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <img src={images} alt=""/>
-      <p>Hello world</p>
-      <button onClick={handleFetch}>Get Random Cat</button>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img className="w-full" src={images} alt=""/>
+          <p>Hello world</p>
+          <button onClick={handleFetch}>Get Random Cat</button>
+        </div>  
+        <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by image.user
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            image.views
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            image.downloads
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            image.likes
+          </li>
+        </ul>
+      </div>      
+      </div>
     </div>
   );
 };
